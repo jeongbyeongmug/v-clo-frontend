dbdda9d50da7d913e7cb08c0b771a00abd8033ba
산악애호가 <xxoa000@gmail.com>
2026-02-04 17:14:09 +0900

    Update nav link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,10 +13,10 @@ export default function Nav({isLogin, onLogout}) {
   // 카테고리 데이터
   const categories = [
     { name: '모델', sub: ['하정', '다희', '순영', '병묵', '성준'] },
-    { name: <NavLink to='/productList?category=outer'>Outer</NavLink>, sub: []},
-    { name: <NavLink to='/productList?category=top'>Top</NavLink>, sub: [] },
-    { name: <NavLink to='/productList?category=bottom'>Bottom</NavLink>,sub: []},
-    { name: <NavLink to='/productList?category=accessory'>Accessory</NavLink>, sub: []}]
+    { name: 'Outer', path:'?category=outer' , sub: []},
+    { name: 'Top', path:'?category=top', sub: [] },
+    { name: 'Bottom', path:'?category=bottom', sub: []},
+    { name: 'Accessory', path:'?category=accessory', sub: []}]
 
   return (
     <div className="nav">
@@ -32,6 +32,10 @@ export default function Nav({isLogin, onLogout}) {
                 onMouseEnter={() => setActiveMenu(c.name)}
                 onMouseLeave={() => setActiveMenu(null)}
               >
+
+                {/* navlink가 있으면 path로 이동, 없으면 이름만 출력 */}
+                {c.path ? <NavLink to={c.path}></NavLink> : c.name}
+                
                 {c.name}
                 {/* 드롭다운 메뉴 */}
                 {activeMenu === c.name && (
